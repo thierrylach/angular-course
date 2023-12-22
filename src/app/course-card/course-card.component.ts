571eb36e60c49c9dd194f7d75f1484f6d7cae52f
Thierry Lach <thierry.lach@bbdo.com>
2023-12-22 10:50:35 -0500

    before lesson 11

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'course-card',
@@ -10,12 +11,22 @@ import { CommonModule } from '@angular/common';
 })
 export class CourseCardComponent implements OnInit {
 
+  @Input({required: true})
+  course: Course;
+
+  @Output('courseSelected')
+  courseEmitter = new EventEmitter<Course>();
+
   constructor() {
 
   }
 
   ngOnInit() {
 
+  }
+
+  onCourseViewed() {
+    this.courseEmitter.emit(this.course);
   }
 
 }
